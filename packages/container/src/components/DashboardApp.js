454738460fc7553591c27e3a4763318fc1d67a95
Diogo Furmann <diogo.furmann@h2oinnovation.com.br>
2025-02-04 13:36:14 -0300

    fix dashboard and remove oci

--- packages/container/src/components/DashboardApp.js
+++ packages/container/src/components/DashboardApp.js
@@ -1,11 +1,25 @@
 import { mount } from 'dashboard/DashboardApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+      onSignIn,
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
